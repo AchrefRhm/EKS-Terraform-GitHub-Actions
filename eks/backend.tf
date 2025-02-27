7aa6727cfc2b6138c1594252d00b44f0d14fefe0
AchrefRhm <116440967+AchrefRhm@users.noreply.github.com>
2025-02-27 16:00:22 +0100

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.9.3, <= 1.11.0"
-  required_providers {
+required_version = "=1.5"
+required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.49.0"
+      version = "=5.10.0"
     }
   }
   backend "s3" {
